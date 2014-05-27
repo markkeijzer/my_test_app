2458db070f692d5a94aacf9a715ab66fca5bd543
Prelang Builder <builder@prelang.com>
2014-05-27 21:36:26 +0000

    Generated scaffolding for model 'Product' via Rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 MyTestApp::Application.routes.draw do
+  resources :products
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}, skip: [:sessions, :registrations]
